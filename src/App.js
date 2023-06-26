dcf858650c3361dde653254cde2ceaaed8634ce2
Aliaksandr <jonnydukalez@gmail.com>
2023-06-26 16:13:28 +0300

    add input and textarea components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
+import {useState} from "react";
+
 import Menu from "./components/menu/menu";
 import Tabs from "./components/tabs/tabs";
+import Input from "./components/form/input/input";
+import Textarea from "./components/form/textarea/textarea";
 import {TABS} from "./constants/tabs";
 
 import './App.scss';
-import {useState} from "react";
+
+const user = {
+    firstName: "Alex",
+    lastName: "Bla"
+}
+
+const formDefaultValues = {
+    name: {key: 'input', value: 'test1', label: 'Name'},
+    msg: {key: 'textarea', value: 'test2', label: 'Message'}
+}
+
 
 function App() {
     const  [activeTab, setActiveTab] = useState(TABS[0]?.key);
-  const user = {
-    firstName: "Alex",
-    lastName: "Bla"
-  }
+    const [formValues, setFormValues] = useState(formDefaultValues);
+    const setNewValue = (key, value) => {
+        setFormValues({
+            ...formValues,
+            [key]: { ...formValues[key], value: value }
+        })
+    }
+
   return (
       <div className="App">
         <Menu user={user}/>
           <Tabs activeTab={activeTab} setActiveTab={setActiveTab} tabs={TABS}/>
+          <Input value={formValues.name} label={formValues.name.label} onChange={setNewValue}/>
+          <Textarea value={formValues.msg} label={formValues.msg.label} onChange={setNewValue}/>
       </div>
   );
 }
---
+++ src/components/form/input/input.js
@@ -0,0 +1,32 @@
+import "./input.scss"
+const Input = (props) => {
+    const {
+        value: {value, key},
+        onChange,
+        disabled,
+        error,
+        errorMsg,
+        label,
+        type = 'text'
+    } = props;
+
+    return(
+        <div className='ra-input__container'>
+            <label className="ra-input__label">
+                <span>{label}</span>
+                <input
+                    className={`ra-input ${error ? 'ra-input--error' : ''}`}
+                    value={value}
+                    disabled={disabled}
+                    onChange={({target: {value}}) => onChange(key, value)}
+                    type={type}
+                />
+            </label>
+            <p className="ra-input__error-list">
+                { errorMsg }
+            </p>
+        </div>
+    )
+}
+
+export default Input;
